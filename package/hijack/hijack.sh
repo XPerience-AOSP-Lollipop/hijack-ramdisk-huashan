72ae30dcdd2437896dbcecebfd1a4e500531102c
bagyusz <phelyx92@gmail.com>
2014-04-30 01:45:46 +0200

    Bringup for cm-11.0_LBL
    and merged dh.harald latest hijack source

--- package/hijack/hijack.sh
+++ package/hijack/hijack.sh
@@ -18,7 +18,21 @@ LED_BLUE_CURRENT="/sys/class/leds/LED1_B/led_current"
 LED_GREEN="/sys/class/leds/LED1_G/brightness"
 LED_GREEN_CURRENT="/sys/class/leds/LED1_G/led_current"
 
-cwm_line () {
+LED2_RED="/sys/class/leds/LED2_R/brightness"
+LED2_RED_CURRENT="/sys/class/leds/LED2_R/led_current"
+LED2_BLUE="/sys/class/leds/LED2_B/brightness"
+LED2_BLUE_CURRENT="/sys/class/leds/LED2_B/led_current"
+LED2_GREEN="/sys/class/leds/LED2_G/brightness"
+LED2_GREEN_CURRENT="/sys/class/leds/LED2_G/led_current"
+
+LED3_RED="/sys/class/leds/LED3_R/brightness"
+LED3_RED_CURRENT="/sys/class/leds/LED3_R/led_current"
+LED3_BLUE="/sys/class/leds/LED3_B/brightness"
+LED3_BLUE_CURRENT="/sys/class/leds/LED3_B/led_current"
+LED3_GREEN="/sys/class/leds/LED3_G/brightness"
+LED3_GREEN_CURRENT="/sys/class/leds/LED3_G/led_current"
+
+philz_ramdisk () {
 	mount -o remount,rw rootfs /
 	cd /
 # Stop services
@@ -48,6 +62,88 @@ cwm_line () {
 		kill -9 $RUNNINGPRC
 	done
 
+	umount -l /system
+	umount -l /data
+	umount -l /mnt/idd
+	umount -l /cache
+	umount -l /lta-label
+
+        umount -l /storage/sdcard1
+
+	umount -l /mnt/obb
+	umount -l /mnt/asec
+	umount -l /mnt/secure/staging
+	umount -l /mnt/secure
+	umount -l /mnt
+	umount -l /acct
+	umount -l /dev/cpuctl
+	umount -l /dev/pts
+        umount -l /cache
+        umount -l /proc
+        umount -l /sys
+
+        rm -vrf /sbin
+        rm -vrf /dev
+        rm -vrf /proc
+        rm -vrf /sys
+        rm -vrf /cache
+	rm sdcard etc init* uevent* default*
+
+        # setup tz.conf for init
+        echo on init > /tz.conf
+        echo export TZ "$(getprop persist.sys.timezone)" >> /tz.conf
+        chmod 750 /tz.conf
+        cp /sbin/zoneinfo.tar /
+
+}
+
+#-----------------------------------------------------------------------------------------------
+
+cwm_ramdisk () {
+	mount -o remount,rw rootfs /
+	cd /
+# Stop services
+
+# Initialize system clock.
+        ### set timezone...
+        export TZ="$(getprop persist.sys.timezone)"
+        ### start time_daemon...
+        /system/bin/time_daemon &
+        sleep 5
+        ### kill time_daemon...
+        kill -9 $(ps | grep time_daemon | grep -v grep | awk -F' ' '{print $1}')
+
+	for SVCRUNNING in $(getprop | grep -E '^\[init\.svc\..*\]: \[running\]' | grep -v ueventd)
+	do
+		SVCNAME=$(expr ${SVCRUNNING} : '\[init\.svc\.\(.*\)\]:.*')
+		stop ${SVCNAME}
+	done
+
+	for RUNNINGPRC in $(ps | grep /system/bin | grep -v grep | grep -v chargemon | awk '{print $1}' ) 
+	do
+		kill -9 $RUNNINGPRC
+	done
+
+	for RUNNINGPRC in $(ps | grep /sbin/ | grep -v grep | awk '{print $1}' )
+	do
+		kill -9 $RUNNINGPRC
+	done
+
+	umount /system
+	umount /data
+	umount /mnt/idd
+	umount /cache
+	umount /lta-label
+
+	umount /mnt/obb
+	umount /mnt/asec
+	umount /mnt/secure/staging
+	umount /mnt/secure
+	umount /mnt
+	umount /acct
+	umount /dev/cpuctl
+	umount /dev/pts
+
         rm -r /sbin
 	rm sdcard etc init* uevent* default*
 
@@ -61,36 +157,29 @@ cwm_line () {
 
 #-----------------------------------------------------------------------------------------------
 
-ramdisk_line () {
+cm_ramdisk () {
 	mount -o remount,rw rootfs /
 	cd /
 # Stop services
 
-        #ps > /temp/ps.txt
-
-	for SVCRUNNING in $(getprop | grep -E '^\[init\.svc\..*\]: \[running\]' )
+	for SVCNAME in $(getprop | grep -E '^\[init\.svc\..*\]: \[running\]' | sed 's/\[init\.svc\.\(.*\)\]:.*/\1/g;')
 	do
-		SVCNAME=$(expr ${SVCRUNNING} : '\[init\.svc\.\(.*\)\]:.*')
-		stop ${SVCNAME}
-		killall -9 ${SVCNAME}
+		stop $SVCNAME
 	done
 
 	for RUNNINGPRC in $(ps | grep /system/bin | grep -v grep | grep -v wipedata | awk '{print $1}' ) 
 	do
-		killall -9 $RUNNINGPRC
+		kill -9 $RUNNINGPRC
 	done
 
 	for RUNNINGPRC in $(ps | grep /sbin | grep -v grep | awk '{print $1}' )
 	do
-		killall -9 $RUNNINGPRC
+		kill -9 $RUNNINGPRC
 	done
 
-        #ps > /temp/ps2.txt
-
+	sync
 
         # umount
-
-        #mount > /temp/mount.txt
 
         ## /boot/modem_fs1
         umount -l /dev/block/mmcblk0p6
@@ -108,6 +197,8 @@ ramdisk_line () {
         umount -l /dev/block/mmcblk0p12
         ## /sdcard (External)
         umount -l /dev/block/mmcblk1p1
+
+	sync
 
 	umount /system
 	umount /data
@@ -157,34 +248,52 @@ ramdisk_line () {
 	umount /acct
 	umount /dev/cpuctl
 	umount /dev/pts
-	umount /dev
 	umount /sys/fs/selinux
 	umount /sys/kernel/debug
-	umount /sys
-	umount /proc
 
-
-        #mount > /temp/mount2.txt
+	sync
 
 	# clean /
 	cd /
 	rm -r /sbin
 	rm -r /storage
 	rm -r /mnt
-	rm -f sdcard sdcard1 ext_card sepolicy seapp_contexts property_contexts file_contexts crashtag mr.log fstab* init* ueventd* default*
-
-        #ls > /temp/ls.txt
+	rm -f sdcard sdcard1 ext_card init*
 }
+
+# Turn on GREEN-led.
+echo '128' > $LED3_GREEN_CURRENT
+echo '128' > $LED3_GREEN
+sleep 0.6
+# Turn on white-led.
+echo '128' > $LED_RED_CURRENT
+echo '128' > $LED_BLUE_CURRENT
+echo '128' > $LED_GREEN_CURRENT
+echo '64' > $LED_RED
+echo '128' > $LED_BLUE
+echo '128' > $LED_GREEN
+sleep 0.6
+# Turn on red-led.
+echo '128' > $LED2_RED_CURRENT
+echo '128' > $LED2_RED
+sleep 0.6
+
+# Turn off GREEN-led.
+echo '0' > $LED3_GREEN_CURRENT
+# Turn off white-led.
+echo '0' > $LED_RED_CURRENT
+echo '0' > $LED_BLUE_CURRENT
+echo '0' > $LED_GREEN_CURRENT
+# Turn off RED-led.
+echo '0' > $LED2_RED_CURRENT
+  
+# Turn on blue-led.
+echo '128' > $LED2_BLUE_CURRENT
+echo '128' > $LED2_BLUE
 
 # Trigger short vibration
 echo '200' > /sys/class/timed_output/vibrator/enable
-# Show blue led
-echo '255' > $LED_BLUE
-echo '255' > $LED_BLUE_CURRENT
-echo '0' > $LED_GREEN
-echo '0' > $LED_GREEN_CURRENT
-echo '0' > $LED_RED
-echo '0' > $LED_RED_CURRENT
+
 
 for EVENTDEV in $(ls /dev/input/event*)
 do
@@ -194,37 +303,66 @@ done
 
 sleep 3
 
-for CATPROC in $(ps | grep cat | grep -v grep | awk '{print $1;}')
+for CATPROC in $(ps | grep cat | grep -v grep | awk '{print $1}')
 do
 	kill -9 ${CATPROC}
 done
 
 # turn off leds
-echo '0' > $LED_BLUE
-echo '0' > $LED_BLUE_CURRENT
-echo '0' > $LED_GREEN
-echo '0' > $LED_GREEN_CURRENT
-echo '0' > $LED_RED
-echo '0' > $LED_RED_CURRENT
+echo '0' > $LED2_BLUE
+echo '0' > $LED2_BLUE_CURRENT
 
 sleep 1
 
-hexdump /temp/keyevent* | grep -e '^.* 0001 0072 .... ....$' > /temp/keycheck
+hexdump /temp/keyevent* | grep -e '^.* 0001 0073 .... ....$' > /temp/keycheck_up
+hexdump /temp/keyevent* | grep -e '^.* 0001 0072 .... ....$' > /temp/keycheck_down
 
-# vol+/-, boot recovery
-if [ -s /temp/keycheck -o -e /cache/recovery/boot ]
+# vol+, boot Philz recovery
+if [ -s /temp/keycheck_up -o -e /cache/recovery/boot ]
+then
+
+	# Show blue led
+	echo '128' > $LED_BLUE
+	echo '128' > $LED_BLUE_CURRENT
+	echo '0' > $LED_GREEN
+	echo '0' > $LED_GREEN_CURRENT
+	echo '128' > $LED_RED
+	echo '128' > $LED_RED_CURRENT
+
+	rm /temp/keycheck_up
+	rm /cache/recovery/boot
+	philz_ramdisk
+	cd /
+	tar xf /temp/philz.tar
+	sleep 1
+
+	# turn off leds
+	echo '0' > $LED_BLUE
+	echo '0' > $LED_BLUE_CURRENT
+	echo '0' > $LED_GREEN
+	echo '0' > $LED_GREEN_CURRENT
+	echo '0' > $LED_RED
+	echo '0' > $LED_RED_CURRENT
+
+	exec /init
+	sleep 2
+fi
+
+# vol-, boot CWM recovery
+if [ -s /temp/keycheck_down -o -e /cache/recovery/boot ]
 then
 
 	# Show blue led
 	echo '0' > $LED_BLUE
 	echo '0' > $LED_BLUE_CURRENT
-	echo '255' > $LED_GREEN
-	echo '255' > $LED_GREEN_CURRENT
+	echo '128' > $LED_GREEN
+	echo '128' > $LED_GREEN_CURRENT
 	echo '0' > $LED_RED
 	echo '0' > $LED_RED_CURRENT
 
+	rm /temp/keycheck_down
 	rm /cache/recovery/boot
-	cwm_line
+	cwm_ramdisk
 	cd /
 	tar xf /temp/recovery.tar
 	sleep 1
@@ -239,12 +377,11 @@ then
 
 	chroot / /init
 	sleep 2
-else
-	ramdisk_line
+fi
+
+	cm_ramdisk
 	cd /
 	tar xf /temp/ramdisk.tar
-	sleep 2
+	sync
+	sleep 1
 	chroot / /init
-	sleep 3
-fi
-	
